a6e4e02766b861a2ff0357b344ccc160d595d227
Nicolas Reboredo <nicolasmdf@gmail.com>
2017-06-25 16:38:24 -0300

    0.9

--- js/index.js
+++ js/index.js
@@ -4,3 +4,8 @@ nosotros (pics)
 video
 contacto
 proximas fechas */
+
+//either navbar-fixed, a collapse-button or the whole thing as a slide menu are my options
+$(document).ready(function() {
+    $(".button-collapse").sideNav()
+})
